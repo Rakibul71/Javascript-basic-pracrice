4be1e24505e42d206305f05c1146b854da837954
Rakibul71 <rakibulislam202019@gmail.com>
2021-08-03 21:59:37 +0600

    array

---
+++ array/adding.js
@@ -0,0 +1,13 @@
+const numbers = [3, 4, 7];
+
+// adding element at the end
+numbers.push(17);
+console.log(numbers);
+
+// adding element at the Begging
+numbers.unshift(71);
+console.log(numbers);
+
+// adding element at the middle
+numbers.splice(2, 0, "rakib", "sadia");
+console.log(numbers);
